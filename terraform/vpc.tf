1b9b63616fdb98c02ad04032d92f18734d768914
GokulakrishnanRavichandra <144846841+GokulakrishnanRavichandra@users.noreply.github.com>
2024-02-20 04:38:03 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -13,7 +13,6 @@ resource "google_compute_subnetwork" "subnet" {
   name          = var.VPC_COUNT == 1 ? var.SUBNET[count.index%length(var.SUBNET)].name : "${var.SUBNET[count.index%length(var.SUBNET)].name}-${floor(count.index / length(var.SUBNET))}${count.index%length(var.SUBNET)}"
   network       = google_compute_network.vpc[floor(count.index / length(var.SUBNET))].id
   region        = var.REGION
-}
 
 resource "google_compute_route" "route" {
   count            = var.VPC_COUNT * length(var.SUBNET)
